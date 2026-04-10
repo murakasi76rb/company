891598fae724fe9496a7f343d900b5b8dfb21263
murakasi <murakasi.ruslan@gmail.com>
2026-04-10 10:19:44 +0200

    add forms.py and create ModelForm for Employee

---
+++ employees/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from employees.models import Employee
+
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = ['name', 'email']
+
+
+    def clean_email(self):
+        BLOCKED_DOMEN: list[str] = ['yandex.ru', 'ya.ru',]
+        email:str = self.cleaned_data['email']
+        domain: str = email.split('@')[-1]
+        if domain in BLOCKED_DOMEN:
+            raise forms.ValidationError("This domen email blocked")
+        return email
