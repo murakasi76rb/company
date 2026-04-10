9160acbd3544e89e7dd8b5c09defef1db87aeba8
murakasi <murakasi.ruslan@gmail.com>
2026-04-10 12:06:51 +0200

    add EmployeeCreateView in views.py

--- employees/views.py
+++ employees/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from employees.models import Employee
 from employees.forms import EmployeeForm
 
@@ -9,4 +10,10 @@ from employees.forms import EmployeeForm
 class EmployeeListView(ListView):
     model = Employee
     template_name = 'employees/employee.html'
-    
+
+
+class EmployeeCreateView(CreateView):
+    model = Employee
+    form_class = EmployeeForm
+    template_name = 'employess/add-employee.html'
+    success_url = 'employees:list'
